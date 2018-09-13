289946907cd65e4cc38f36f6750c6384f7421329
Sonja Weichler <weichler@informatik.uni-hamburg.de>
2018-09-13 16:25:53 +0200

    ODSH-(16,36,37,38) : loads the decoupled configuration file. Comments added

--- ckanext/odsh/harvesters/ckan_mapper.py
+++ ckanext/odsh/harvesters/ckan_mapper.py
@@ -1,47 +1,31 @@
+#!/usr/bin/env python
+"""
+Mapper, that loads all necessary mapping files, calls the pyjq method and returns the value.
+"""
 import json
 import pyjq as pyjq
 
-# Save all mappings from Statistikamt Nord numbers to MDR authority codes, which will be used to determine the CKAN-groups
-with open('/usr/lib/ckan/default/src/ckanext-odsh/ckanext/odsh/harvesters/number_dcat_de.json') as f:
-    numbers = json.load(f)
+# import the "stat_amt_nord" configuration file to determine
+# 1st the mapping from statistikamt nord fields onto ckan-fields and
+# 2nd the mapping form the category numbers onto the MDR data-theme authority codes
+import config_stat_amt_nord as sta_amt_nord
 
-# Filter string needed by the pyjq-mapper, which is configured for the value we retrieve from Statistikamt Nord
-config_filter = '{"author" : .VeroeffentlichendeStelle.Name,' \
-                '"url" : .WeitereInformationen.URL,' \
-                '"notes" : .Beschreibung,' \
-                '"title" : .Titel,' \
-                '"author_email": .VeroeffentlichendeStelle.EMailAdresse,' \
-                '"extra": [' \
-                '   {"key": "identifier", "value": (.Quelle + ":" +.DokumentID)},' \
-                '   {"key": "issued", "value": .Veroeffentlichungsdatum?},' \
-                '   {"key": "modified", "value": .Aenderungsdatum?},' \
-                '   {"key": "temporal_start", "value": .ZeitraumVon?},' \
-                '   {"key": "temporal_end", "value": .ZeitraumBis?}],' \
-                '"resources" : [.Ressourcen.Ressource[] | {' \
-                '   "url":.URLZumDownload,' \
-                '   "size" : 50,' \
-                '   "name" : .Ressourcenname,' \
-                '   "format": .Format.FormatTyp }],' \
-                '"license_id" : .Nutzungsbestimmungen.ID_derLizenz,' \
-                '"type" : "dataset",' \
-                '"tags" : [.Schlagwoerter |.Schlagwort[] | gsub(", "; ",") | select(length > 0) |' \
-                '   split(",") |.[] |.+ "" | if (. | length) <= 100 then {"name" : .} else empty end],' \
-                '"groups" : [$numbers[.StANKategorie][]? | {"name" : .}]}'
 
-#"file_size" : (.Dateigroesse | tonumber | . * 1000),' \
-
-def pyjq_mapper(value):
-    print "value: " + str(value)
-
+def pyjq_mapper(config_filter, value, numbers):
+    # print "value: " + str(value)
     tmp = pyjq.all(config_filter, value, vars={"numbers": numbers})
 
-    print "tmp: " + str(tmp)
-    return tmp[0]
+    # print "tmp cm: " + str(tmp)
+    return dict(tmp[0])
 
-# for testing reasons only
+
+# for very little testing reasons regarding the pyjq lib only
+test_config_filter = sta_amt_nord.config_filter
+test_numbers = sta_amt_nord.numbers
+
 with open('/usr/lib/ckan/default/src/ckanext-odsh/ckanext/odsh/harvesters/statistik-nord-example_2.json') as f:
     input = json.load(f)
-    print input
-    result = dict(pyjq_mapper(input))
-print result
+    print "Input: " + str(input)
+    result = dict(pyjq_mapper(test_config_filter, input, test_numbers))
+print "Result: " + str(result)
 
