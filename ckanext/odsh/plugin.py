fbfec98e1f6af3c4bd3db63fb2a06132ecd6086e
chbaeh <chbaeh@gmail.com>
2018-09-13 13:22:57 +0200

    Adds proof of concept for using helper functions in html templates

--- ckanext/odsh/plugin.py
+++ ckanext/odsh/plugin.py
@@ -1,9 +1,19 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+def odsh_main_groups():
+    '''Return a list of the groups to be shown on the start page.'''
+
+    # Get a list of all the site's groups from CKAN, sorted by number of
+    # datasets.
+    groups = toolkit.get_action('group_list')(
+        data_dict={'all_fields': True})
+
+    return groups
 
 class OdshPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +21,9 @@ class OdshPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'odsh')
+    
+    def get_helpers(self):
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'odsh_main_groups': odsh_main_groups}
