634e5be71d3318503ce9e855ab1afd3dd28a22b3
anonymous <anonymous>
2018-11-02 10:33:08 +0100

    Fixes helper function for score

--- ckanext/odsh/helpers.py
+++ ckanext/odsh/helpers.py
@@ -7,19 +7,17 @@ log = logging.getLogger(__name__)
 
 def odsh_openness_score_dataset_html(dataset):
     score = 0
-    #dataset = json.loads(dataset)
     resources = dataset.get('resources')
-    if resources is None:
-        return 0
-    for resource in resources:
-        r_qa = resource.get('qa')
-        if r_qa:
-            try:
-                qa = ast.literal_eval()
-                resource_score = qa.get('openness_score')
-                if resource_score > score:
-                    score = resource_score
-            except AttributeError, e:
-                log.error('Error while calculating openness score %s: %s\nException: %s',
-                    e.__class__.__name__,  unicode(e), traceback.format_exc())
+    if resources:
+        for resource in resources:
+            r_qa = resource.get('qa')
+            if r_qa:
+                try:
+                    qa = ast.literal_eval(r_qa)
+                    resource_score = qa.get('openness_score')
+                    if resource_score > score:
+                        score = resource_score
+                except AttributeError, e:
+                    log.error('Error while calculating openness score %s: %s\nException: %s',
+                        e.__class__.__name__,  unicode(e), traceback.format_exc())
     return score
