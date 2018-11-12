01677623c3d34cb8d200c1cec4e7261a6eab97ca
anonymous <anonymous>
2018-11-12 15:57:10 +0100

    ODPSH-89: change login and dashboard redirect

--- ckanext/odsh/controller.py
+++ ckanext/odsh/controller.py
@@ -1,6 +1,25 @@
 import ckan.lib.base as base
 from ckan.controllers.home import HomeController
+from ckan.controllers.user import UserController
+import ckan.lib.helpers as h
+import ckan.authz as authz
+from ckan.common import c
+
+abort = base.abort
 
 class OdshRouteController(HomeController):
     def info_page(self):
         return base.render('info_page.html')
+
+class OdshUserController(UserController):
+    def me(self, locale=None):
+        if not c.user:
+            h.redirect_to(locale=locale, controller='user', action='login',
+                          id=None)
+        user_ref = c.userobj.get_reference_preferred_for_uri()
+        h.redirect_to(locale=locale, controller='package', action='search')
+
+    def dashboard(self, id=None, offset=0):
+        if not authz.is_sysadmin(c.user):
+            abort(404)
+        return super(OdshUserController,self).dashboard(id,offset)
