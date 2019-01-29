ca79a8b38e746ae94651b9ea2f91ce57ed84a4b7
chbaeh <chbaeh@gmail.com>
2019-01-29 13:31:16 +0100

    fix value extraction from extras

--- ckanext/odsh/helpers.py
+++ ckanext/odsh/helpers.py
@@ -5,8 +5,8 @@ import ckan.plugins.toolkit as toolkit
 import ckan.logic as logic
 import ckan.model as model
 import json
-from ckan.common import  c
-import  datetime
+from ckan.common import c
+import datetime
 from dateutil import parser
 from ckan.common import config
 import urllib
@@ -14,6 +14,7 @@ import hashlib
 
 get_action = logic.get_action
 log = logging.getLogger(__name__)
+
 
 def odsh_openness_score_dataset_html(dataset):
     score = 0
@@ -26,7 +27,8 @@ def odsh_openness_score_dataset_html(dataset):
         if r_qa:
             try:
                 qa = None
-                if isinstance(r_qa, basestring): # r_qa might be a string of a dictionary when 'dataset' is send from solr
+                # r_qa might be a string of a dictionary when 'dataset' is send from solr
+                if isinstance(r_qa, basestring):
                     qa = ast.literal_eval(r_qa)
                 else:
                     qa = r_qa
@@ -35,84 +37,93 @@ def odsh_openness_score_dataset_html(dataset):
                     score = resource_score
             except AttributeError, e:
                 log.error('Error while calculating openness score %s: %s\nException: %s',
-                    e.__class__.__name__,  unicode(e), traceback.format_exc())
+                          e.__class__.__name__,  unicode(e), traceback.format_exc())
     return score
+
 
 def odsh_get_resource_details(resource_id):
     resource_details = toolkit.get_action('resource_show')(
         data_dict={'id': resource_id})
     return resource_details
-    
+
 
 def odsh_get_resource_views(pkg_dict, resource):
 
-        context = {'model': model, 'session': model.Session,
-                   'user': c.user, 'for_view': True,
-                   'auth_user_obj': c.userobj}
-        return get_action('resource_view_list')(
-            context, {'id': resource['id']})
+    context = {'model': model, 'session': model.Session,
+               'user': c.user, 'for_view': True,
+               'auth_user_obj': c.userobj}
+    return get_action('resource_view_list')(
+        context, {'id': resource['id']})
+
 
 def odsh_get_bounding_box(pkg_dict):
     try:
-        extras=pkg_dict.get('extras')
-        spatial=None
+        extras = pkg_dict.get('extras')
+        spatial = None
         for f in extras:
             if 'key' in f and f['key'] == 'spatial':
-                spatial=f['value']
+                spatial = f['value']
                 break
 
         if spatial is not None:
             d = json.loads(spatial)
             if 'coordinates' in d:
-                coords=d['coordinates']
+                coords = d['coordinates']
                 return compute_bounding_box(coords)
     except Exception, e:
         log.error('Error while bounding box %s: %s\nException: %s',
-            e.__class__.__name__,  unicode(e), traceback.format_exc())
-    return None 
+                  e.__class__.__name__,  unicode(e), traceback.format_exc())
+    return None
+
 
 def compute_bounding_box(coords):
-    if len(coords)==0:
+    if len(coords) == 0:
         return None
 
     coords = [c for sublist in coords for c in sublist]
     if type(coords[0][0]) == list:
-        ## multipolygon
+        # multipolygon
         coords = [c for sublist in coords for c in sublist]
 
-    minx = min(coords, key = lambda t: t[0])[0]
-    maxx = max(coords, key = lambda t: t[0])[0]
-    miny = min(coords, key = lambda t: t[1])[1]
-    maxy = max(coords, key = lambda t: t[1])[1]
-     
+    minx = min(coords, key=lambda t: t[0])[0]
+    maxx = max(coords, key=lambda t: t[0])[0]
+    miny = min(coords, key=lambda t: t[1])[1]
+    maxy = max(coords, key=lambda t: t[1])[1]
+
     return [maxx, minx, maxy, miny]
 
+
 def odsh_get_spatial_text(pkg_dict):
-    extras=pkg_dict.get('extras')
-    spatial=None
+    extras = pkg_dict.get('extras')
+    spatial = None
     if extras is None:
-        return None 
+        return None
     for f in extras:
         if 'key' in f and f['key'] == 'spatial_text':
-            spatial=f['value']
+            spatial = f['value']
             return spatial
-    return None 
+    return None
+
 
 def odsh_render_datetime(datetime_, date_format='{0.day:02d}.{0.month:02d}.{0.year:4d}'):
     dt = parser.parse(datetime_)
     return date_format.format(dt)
+
 
 def odsh_upload_known_formats():
     value = config.get('ckanext.odsh.upload_formats', [])
     value = toolkit.aslist(value)
     return value
 
+
 def odsh_encodeurl(url):
     return urllib.quote(url, safe='')
+
 
 def odsh_create_checksum(in_string):
     hashstring = hashlib.md5(in_string.encode('utf-8')).hexdigest()
     return int(hashstring, base=16)
+
 
 def odsh_extract_error(key, errors):
     if not errors or not ('extras' in errors):
@@ -122,11 +133,14 @@ def odsh_extract_error(key, errors):
         if 'key' in item:
             for error in item['key']:
                 if error.startswith(key):
-                    return error.replace(key+':','')
+                    return error.replace(key+':', '')
+
 
 def odsh_extract_value_from_extras(extras, key):
     if not extras:
         return None
     for item in extras:
         if 'key' in item and item['key'].lower() == key.lower():
-            return item['value']
+            if 'value' in item:
+                return item['value']
+            return None
--- ckanext/odsh/tests/test_upload.py
+++ ckanext/odsh/tests/test_upload.py
@@ -18,7 +18,6 @@ class TestUpload(helpers.FunctionalTestBase):
     def teardown(self):
         model.repo.rebuild_db()
 
-
     @odsh_test()
     def test_upload_empty_form_fails(self):
         # arrange
@@ -39,7 +38,7 @@ class TestUpload(helpers.FunctionalTestBase):
         form = self._get_package_new_form()
 
         # act
-        form[self._get_field_name('spatial_uri')]='wrong'
+        form[self._get_field_name('spatial_uri')] = 'wrong'
         response = self._submit_form(form)
 
         # assert
@@ -51,20 +50,20 @@ class TestUpload(helpers.FunctionalTestBase):
         form = self._get_package_new_form()
 
         # act
-        form[self._get_field_name('temporal_start')]='2001-12-35'
+        form[self._get_field_name('temporal_start')] = '2001-12-35'
         response1 = self._submit_form(form)
-        form[self._get_field_name('temporal_end')]='2001-12-35'
+        form[self._get_field_name('temporal_end')] = '2001-12-35'
         response2 = self._submit_form(form)
-        form[self._get_field_name('temporal_start')]='2001-12-01'
+        form[self._get_field_name('temporal_start')] = '2001-12-01'
         response3 = self._submit_form(form)
-        form[self._get_field_name('temporal_end')]='2001-12-01'
-        form[self._get_field_name('temporal_start')]='2001-12-35'
+        form[self._get_field_name('temporal_end')] = '2001-12-01'
+        form[self._get_field_name('temporal_start')] = '2001-12-35'
         response4 = self._submit_form(form)
-        form[self._get_field_name('temporal_start')]='11-11-11'
+        form[self._get_field_name('temporal_start')] = '11-11-11'
         response5 = self._submit_form(form)
-        form[self._get_field_name('temporal_start')]='11-11-2011'
+        form[self._get_field_name('temporal_start')] = '11-11-2011'
         response6 = self._submit_form(form)
-        form[self._get_field_name('temporal_start')]='datum'
+        form[self._get_field_name('temporal_start')] = 'datum'
         response7 = self._submit_form(form)
 
         # assert
@@ -75,17 +74,53 @@ class TestUpload(helpers.FunctionalTestBase):
         response5.mustcontain('odsh_temporal_start_not_date_error_label')
         response6.mustcontain('odsh_temporal_start_not_date_error_label')
 
+    @odsh_test()
+    def test_upload_license_without_attribution_text(self):
+        # arrange
+        form = self._get_package_new_form()
+        form['title'] = 'Titel'
+        form['name'] = 'sdlkjsadlfkjas'
+        form['notes'] = 'notes'
+        form['owner_org'] = self.org['id']
+        form[self._get_field_name(
+            'spatial_uri')] = 'http://dcat-ap.de/def/politicalGeocoding/districtKey/01001'
+        form[self._get_field_name('issued')] = '2019-01-29'
+        form[self._get_field_name('temporal_start')] = '2019-01-29'
+        form[self._get_field_name('temporal_end')] = '2019-02-02'
+        form['license_id'] = 'http://dcat-ap.de/def/licenses/dl-zero-de/2.0'
+        # act
+        print(form)
+        form[self._get_field_key('licenseAttributionByText')
+             ] = 'licenseAttributionByText'
+        form[self._get_field_name('licenseAttributionByText')] = None
+        response = self._submit_and_follow_form(form)
+
+        # assert
+        response.mustcontain('odsh_spatial_uri_unknown_error_label')
 
     def _get_field_name(self, field):
         checksum = odsh_create_checksum(field)
         return 'extras__' + str(checksum) + '__value'
 
+    def _get_field_key(self, field):
+        checksum = odsh_create_checksum(field)
+        return 'extras__' + str(checksum) + '__key'
+
     def _submit_form(self, form):
         return webtest_submit(form, 'save', status=200, extra_environ=self.env)
+
+    def _submit_and_follow_form(self, form):
+        app = self._get_test_app()
+        # return webtest_submit(form, 'save', status=200, extra_environ=self.env)
+        return helpers.submit_and_follow(app, form, self.env, 'save')
 
     def _get_package_new_form(self):
         app = self._get_test_app()
         user = factories.User()
+        self.org = factories.Organization(
+            name="my-org",
+            users=[{'name': user['id'], 'capacity': 'admin'}]
+        )
         self.env = {'REMOTE_USER': user['name'].encode('ascii')}
         response = app.get(
             url=url_for(controller='package', action='new'),
