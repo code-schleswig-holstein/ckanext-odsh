3119aea3af10a66d7df91e8140fcd96065b33968
anonymous <anonymous>
2019-02-20 12:01:12 +0100

    add locale for datepicker

--- ckanext/odsh/fanstatic/odsh_datepicker.js
+++ ckanext/odsh/fanstatic/odsh_datepicker.js
@@ -3,13 +3,27 @@ this.ckan.module('odsh_datepicker', function ($, _)
     return {
         initialize: function ()
         {
+            if (!$.fn.datepicker.dates['de'])
+                $.fn.datepicker.dates['de'] = {
+                    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+                    daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
+                    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+                    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+                    monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+                    today: "Heute",
+                    monthsTitle: "Monate",
+                    clear: "Löschen",
+                    weekStart: 1,
+                    format: "dd.mm.yyyy"
+                };
             var showFormat = "dd.mm.yyyy"
             var opts = {
                 format: showFormat,
                 // startView: 3,
                 // minViewMode: 2,
                 // keyboardNavigation: false,
-                autoclose: true
+                autoclose: true,
+                language: 'de'
             }
             var onChange = function (ev)
             {
