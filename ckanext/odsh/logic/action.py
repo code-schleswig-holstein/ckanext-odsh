540603872170ff6e8d8f3080842ea1e2a0ef1d5c
Dennis <2rupnow@informatik.uni-hamburg.de>
2019-04-01 14:29:37 +0200

    adds own user_create handling

--- ckanext/odsh/logic/action.py
+++ ckanext/odsh/logic/action.py
@@ -1,17 +1,17 @@
 import logging
-from ckan.logic.action.create import package_create
+from ckan.logic.action.create import package_create, user_create, group_member_create
 import ckan.model as model
+import ckan.lib.dictization.model_dictize as model_dictize
 
 log = logging.getLogger(__name__)
 
 
 def odsh_package_create(context, data_dict):
-    log.debug('in ODSH package_create')
     munge_increment_name(data_dict)
     return package_create(context, data_dict)
 
+
 def munge_increment_name(data_dict):
-    log.debug('IN MUNGE')
     from ckan.lib.munge import munge_title_to_name
 
     name_base = name = munge_title_to_name(data_dict['title'])
@@ -23,3 +23,10 @@ def munge_increment_name(data_dict):
         pkg = model.Package.get(name)
     log.debug('name: %s' % name)
     data_dict['name'] = name
+
+
+def odsh_user_create(context, data_dict):
+    model = context['model']
+    user = user_create(context, data_dict)
+    a = group_member_create(context, {'id': 'educ', 'username': user.get('username'), 'role': 'editor'})
+    return model_dictize.user_dictize(model.User.get(user.get('username')), context)
