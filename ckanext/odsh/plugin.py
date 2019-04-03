d69b62a587442057e8dce8b73eaf6e62c9b90536
Dennis <2rupnow@informatik.uni-hamburg.de>
2019-04-03 13:06:11 +0200

    adds missing map return

--- ckanext/odsh/plugin.py
+++ ckanext/odsh/plugin.py
@@ -90,6 +90,7 @@ class OdshAutocompletePlugin(plugins.SingletonPlugin):
     def before_map(self, map):
         controller = 'ckanext.odsh.controller:OdshAutocompleteController'
         map.connect('/autocomplete', controller=controller, action='autocomplete')
+        return map
 
 
 class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm):
