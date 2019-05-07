45bdc1dfd67ffe296c82556c7055fef3d4eabf57
anonymous <anonymous>
2019-05-07 23:03:46 +0200

    Dcat-datasets will be ignored when harvesting, if their modified-date is older than the same already existing dataset

--- ckanext/odsh/plugin.py
+++ ckanext/odsh/plugin.py
@@ -12,10 +12,12 @@ import ckan.lib.helpers as helpers
 import helpers as odsh_helpers
 import ckanext.odsh.logic.action as action
 from ckanext.dcat.interfaces import IDCATRDFHarvester
+from ckanext.dcatde.extras import Extras
 
 from routes.mapper import SubMapper
 from pylons import config
 from dateutil.parser import parse
+from ckan import model
 
 import ckan.plugins as p
 
@@ -207,9 +209,36 @@ class OdshDCATHarvestPlugin(plugins.SingletonPlugin):
     plugins.implements(IDCATRDFHarvester, inherit=True)
 
     def before_update(self, harvest_object, dataset_dict, temp_dict):
-        dataset_dict['title']="Title"
-        print("BEFORE update")
-        # self._before(dataset_dict, temp_dict, harvest_object)
+        
+        existing_package_dict = self._get_existing_dataset(harvest_object.guid)
+        new_dataset_extras = Extras(dataset_dict['extras'])
+        if new_dataset_extras.key('modified') and \
+          new_dataset_extras.value('modified') < existing_package_dict.get('metadata_modified'):
+            log.info("Modified date of new dataset is not newer than "
+            + "the already exisiting dataset, ignoring new one.") 
+            dataset_dict.clear()
+
+    def _get_existing_dataset(self, guid):
+        '''
+        Checks if a dataset with a certain guid extra already exists
+
+        Returns a dict as the ones returned by package_show
+        '''
+
+        datasets = model.Session.query(model.Package.id) \
+                                .join(model.PackageExtra) \
+                                .filter(model.PackageExtra.key == 'guid') \
+                                .filter(model.PackageExtra.value == guid) \
+                                .filter(model.Package.state == 'active') \
+                                .all()
+
+        if not datasets:
+            return None
+        elif len(datasets) > 1:
+            log.error('Found more than one dataset with the same guid: {0}'
+                      .format(guid))
+
+        return p.toolkit.get_action('package_show')({}, {'id': datasets[0][0]})
 
 
 class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm):
