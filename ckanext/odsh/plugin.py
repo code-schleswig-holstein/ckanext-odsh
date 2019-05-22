909dada3d08b7bc9e15698337e93f45fee9065cd
anonymous <anonymous>
2019-05-22 14:36:13 +0200

    prevents occurences of the master IP on the frontend
    http://{IP}/api/i18n/de wouldn't be passed through the apache proxy because it got generated by javascript with the master IP on sites like dataset/new
    
    On the same sites the master IP would be found in the HTML

--- ckanext/odsh/plugin.py
+++ ckanext/odsh/plugin.py
@@ -288,7 +288,8 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
                 'presorted_license_options': odsh_helpers.presorted_license_options,
                 'odsh_tracking_id': odsh_helpers.odsh_tracking_id,
                 'odsh_tracking_url': odsh_helpers.odsh_tracking_url,
-                'odsh_has_more_facets': odsh_helpers.odsh_has_more_facets
+                'odsh_has_more_facets': odsh_helpers.odsh_has_more_facets,
+                'odsh_public_url': odsh_helpers.odsh_public_url
                 }
 
     def after_map(self, map):
