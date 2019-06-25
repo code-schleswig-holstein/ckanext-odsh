b64d9e4402b722a4f640062e97d69f4d004dd21b
anonymous <anonymous>
2019-06-25 10:03:15 +0200

    ODPSH-31: update layout

--- ckanext/odsh/plugin.py
+++ ckanext/odsh/plugin.py
@@ -1,3 +1,4 @@
+from multiline_formatter.formatter import MultilineMessagesFormatter
 import datetime
 import json
 import ckan.plugins as plugins
@@ -24,7 +25,7 @@ import ckan.plugins as p
 import logging
 import validation
 import precondition
- 
+
 import sys
 
 log = logging.getLogger(__name__)
@@ -39,7 +40,7 @@ log = logging.getLogger(__name__)
 #     @wraps(handle_http_exception)
 #     def ret_val(exception):
 #         print("HEHREHR")
-#         exc = handle_http_exception(exception)    
+#         exc = handle_http_exception(exception)
 #         return jsonify({'code':exc.code, 'message':exc.description}), exc.code
 #     return ret_val
 
@@ -56,7 +57,7 @@ log = logging.getLogger(__name__)
 
 _ = toolkit._
 
-from multiline_formatter.formatter import MultilineMessagesFormatter
+
 class OdshLogger(MultilineMessagesFormatter):
     multiline_marker = '...'
     multiline_fmt = multiline_marker + ' : %(message)s'
@@ -79,7 +80,7 @@ class OdshLogger(MultilineMessagesFormatter):
                 self.multiline_fmt % dict(record.__dict__, message=line)
                 for line in splitted
             )
-            output = output.replace('"','\\"')
+            output = output.replace('"', '\\"')
             output += endl_marker
         else:
             output = self._fmt % record.__dict__
@@ -96,7 +97,7 @@ class OdshLogger(MultilineMessagesFormatter):
                     self.multiline_fmt % dict(record.__dict__, message=line)
                     for index, line in enumerate(record.exc_text.splitlines())
                 )
-                output = output.replace('"','\\"')
+                output = output.replace('"', '\\"')
                 output += endl_marker
             except UnicodeError:
                 output += '\n'.join(
@@ -141,9 +142,10 @@ def odsh_group_id_selected(selected, group_id):
 
     return False
 
-def remove_route(map,routename):
+
+def remove_route(map, routename):
     route = None
-    for i,r in enumerate(map.matchlist):
+    for i, r in enumerate(map.matchlist):
 
         if r.name == routename:
             route = r
@@ -178,27 +180,39 @@ class OdshHarvestPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'harvest_templates')
     plugins.implements(plugins.IRoutes, inherit=True)
+
     def before_map(self, map):
-        DATASET_TYPE_NAME='harvest'
+        DATASET_TYPE_NAME = 'harvest'
         controller = 'ckanext.odsh.controller:OdshHarvestController'
 
-        map.connect('{0}_delete'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/delete/:id',controller=controller, action='delete')
-        map.connect('{0}_refresh'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/refresh/:id',controller=controller,
-                action='refresh')
-        map.connect('{0}_admin'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/admin/:id', controller=controller, action='admin')
-        map.connect('{0}_about'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/about/:id', controller=controller, action='about')
-        map.connect('{0}_clear'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/clear/:id', controller=controller, action='clear')
+        map.connect('{0}_delete'.format(DATASET_TYPE_NAME), '/' +
+                    DATASET_TYPE_NAME + '/delete/:id', controller=controller, action='delete')
+        map.connect('{0}_refresh'.format(DATASET_TYPE_NAME), '/' + DATASET_TYPE_NAME + '/refresh/:id', controller=controller,
+                    action='refresh')
+        map.connect('{0}_admin'.format(DATASET_TYPE_NAME), '/' +
+                    DATASET_TYPE_NAME + '/admin/:id', controller=controller, action='admin')
+        map.connect('{0}_about'.format(DATASET_TYPE_NAME), '/' +
+                    DATASET_TYPE_NAME + '/about/:id', controller=controller, action='about')
+        map.connect('{0}_clear'.format(DATASET_TYPE_NAME), '/' +
+                    DATASET_TYPE_NAME + '/clear/:id', controller=controller, action='clear')
 
-        map.connect('harvest_job_list', '/' + DATASET_TYPE_NAME + '/{source}/job', controller=controller, action='list_jobs')
-        map.connect('harvest_job_show_last', '/' + DATASET_TYPE_NAME + '/{source}/job/last', controller=controller, action='show_last_job')
-        map.connect('harvest_job_show', '/' + DATASET_TYPE_NAME + '/{source}/job/{id}', controller=controller, action='show_job')
-        map.connect('harvest_job_abort', '/' + DATASET_TYPE_NAME + '/{source}/job/{id}/abort', controller=controller, action='abort_job')
+        map.connect('harvest_job_list', '/' + DATASET_TYPE_NAME +
+                    '/{source}/job', controller=controller, action='list_jobs')
+        map.connect('harvest_job_show_last', '/' + DATASET_TYPE_NAME +
+                    '/{source}/job/last', controller=controller, action='show_last_job')
+        map.connect('harvest_job_show', '/' + DATASET_TYPE_NAME +
+                    '/{source}/job/{id}', controller=controller, action='show_job')
+        map.connect('harvest_job_abort', '/' + DATASET_TYPE_NAME +
+                    '/{source}/job/{id}/abort', controller=controller, action='abort_job')
 
-        map.connect('harvest_object_show', '/' + DATASET_TYPE_NAME + '/object/:id', controller=controller, action='show_object')
-        map.connect('harvest_object_for_dataset_show', '/dataset/harvest_object/:id', controller=controller, action='show_object', ref_type='dataset')
+        map.connect('harvest_object_show', '/' + DATASET_TYPE_NAME +
+                    '/object/:id', controller=controller, action='show_object')
+        map.connect('harvest_object_for_dataset_show', '/dataset/harvest_object/:id',
+                    controller=controller, action='show_object', ref_type='dataset')
 
         org_controller = 'ckanext.harvest.controllers.organization:OrganizationController'
-        map.connect('{0}_org_list'.format(DATASET_TYPE_NAME), '/organization/' + DATASET_TYPE_NAME + '/' + '{id}', controller=org_controller, action='source_list')
+        map.connect('{0}_org_list'.format(DATASET_TYPE_NAME), '/organization/' +
+                    DATASET_TYPE_NAME + '/' + '{id}', controller=org_controller, action='source_list')
         return map
 
     def after_map(self, map):
@@ -209,13 +223,13 @@ class OdshDCATHarvestPlugin(plugins.SingletonPlugin):
     plugins.implements(IDCATRDFHarvester, inherit=True)
 
     def before_update(self, harvest_object, dataset_dict, temp_dict):
-        
+
         existing_package_dict = self._get_existing_dataset(harvest_object.guid)
         new_dataset_extras = Extras(dataset_dict['extras'])
         if new_dataset_extras.key('modified') and \
-          new_dataset_extras.value('modified') < existing_package_dict.get('metadata_modified'):
+                new_dataset_extras.value('modified') < existing_package_dict.get('metadata_modified'):
             log.info("Modified date of new dataset is not newer than "
-            + "the already exisiting dataset, ignoring new one.") 
+                     + "the already exisiting dataset, ignoring new one.")
             dataset_dict.clear()
 
     def _get_existing_dataset(self, guid):
@@ -311,25 +325,26 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
         if p.toolkit.asbool(config.get('ckanext.dcat.enable_rdf_endpoints', True)):
             remove_route(map, 'dcat_catalog')
             map.connect('dcat_catalog',
-                            config.get('ckanext.dcat.catalog_endpoint', '/catalog.{_format}'),
-                            controller='ckanext.odsh.controller:OdshDCATController', action='read_catalog',
-                            requirements={'_format': 'xml|rdf|n3|ttl|jsonld'})
+                        config.get('ckanext.dcat.catalog_endpoint',
+                                   '/catalog.{_format}'),
+                        controller='ckanext.odsh.controller:OdshDCATController', action='read_catalog',
+                        requirements={'_format': 'xml|rdf|n3|ttl|jsonld'})
 
         # with SubMapper(map, controller='ckanext.odsh.controller:OdshApiController') as m:
         #     m.connect('/catalog2', action='read_catalog')
 
-
-
         # /api ver 3 or none with matomo
         GET_POST = dict(method=['GET', 'POST'])
         with SubMapper(map, controller='ckanext.odsh.controller:OdshApiController', path_prefix='/api{ver:/3|}', ver='/3') as m:
-            m.connect('/action/{logic_function}', action='action', conditions=GET_POST)
+            m.connect('/action/{logic_function}',
+                      action='action', conditions=GET_POST)
 
         with SubMapper(map, controller='ckanext.odsh.controller:OdshFeedController') as m:
             m.connect('/feeds/custom.atom', action='custom')
 
         with SubMapper(map, controller='ckanext.odsh.controller:OdshPackageController') as m:
-            m.connect('new_view', '/dataset/{id}/resource/{resource_id}/new_view', action='edit_view', ckan_icon='pencil-square-o')
+            m.connect('new_view', '/dataset/{id}/resource/{resource_id}/new_view',
+                      action='edit_view', ckan_icon='pencil-square-o')
 
         with SubMapper(map, controller='ckanext.odsh.controller:OdshGroupController') as m:
             m.connect('organizations_index', '/organization', action='index')
@@ -338,7 +353,8 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
         with SubMapper(map, controller='ckanext.odsh.controller:OdshUserController') as m:
             m.connect('user_index', '/user', action='index')
             m.connect('/user/edit', action='edit')
-            m.connect('user_edit', '/user/edit/{id:.*}', action='edit', ckan_icon='cog')
+            m.connect(
+                'user_edit', '/user/edit/{id:.*}', action='edit', ckan_icon='cog')
             m.connect('user_delete', '/user/delete/{id}', action='delete')
             m.connect('/user/reset/{id:.*}', action='perform_reset')
             m.connect('/user/reset', action='request_reset')
@@ -350,6 +366,10 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
             m.connect('/user/logged_out_redirect', action='logged_out_page')
             m.connect('user_datasets', '/user/{id:.*}', action='read',
                       ckan_icon='sitemap')
+
+        map.connect('comment_datarequest', '/datarequest/{id}',
+                    controller='ckanext.datarequests.controllers.ui_controller:DataRequestsUI',
+                    action='comment', conditions=dict(method=['GET', 'POST']), ckan_icon='comment')
         return map
 
     def dataset_facets(self, facets_dict, package_type):
@@ -372,7 +392,7 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
                             'groups': _('Kategorie')})
 
     def _update_schema(self, schema):
-        for field in ['title', 'notes','license_id']:
+        for field in ['title', 'notes', 'license_id']:
             schema.update({field: [toolkit.get_converter('not_empty')]})
 
         for i, item in enumerate(schema['tags']['name']):
@@ -394,10 +414,11 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
                 toolkit.get_converter('validate_licenseAttributionByText'),
             ]
         })
-        schema.update({'__extras':  [toolkit.get_converter('odsh_validate_extras')] })
+        schema.update(
+            {'__extras':  [toolkit.get_converter('odsh_validate_extras')]})
 
-        ## only to make sure the spatial field is there for validation
-        # schema.update({'spatial':  [toolkit.get_converter('convert_from_extras')]}) 
+        # only to make sure the spatial field is there for validation
+        # schema.update({'spatial':  [toolkit.get_converter('convert_from_extras')]})
 
     def create_package_schema(self):
         schema = super(OdshPlugin, self).create_package_schema()
@@ -430,7 +451,7 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
     # use several daterange queries agains temporal_start and temporal_end field
     # TODO: use field of type date_range in solr index instead
     def before_search(self, search_params):
-        search_params['facet.mincount']=0
+        search_params['facet.mincount'] = 0
         extras = search_params.get('extras')
         print(search_params)
         if not extras:
@@ -439,8 +460,8 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
 
         fq = search_params['fq']
 
-        start_date=None
-        end_date=None
+        start_date = None
+        end_date = None
         try:
             start_date = odsh_helpers.extend_search_convert_local_to_utc_timestamp(
                 extras.get('ext_startdate'))
@@ -462,7 +483,6 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
             do_enclosing_query = False
             end_date = '*'
 
-
         start_query = '+extras_temporal_start:[{start_date} TO {end_date}]'.format(
             start_date=start_date, end_date=end_date)
 
@@ -480,37 +500,37 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
             enclosing_query = ' OR ({enclosing_query_start} AND {enclosing_query_end})'.format(
                 enclosing_query_start=enclosing_query_start, enclosing_query_end=enclosing_query_end)
 
-        
         if end_date is '*':
             open_end_query = '(*:* NOT extras_temporal_end:[* TO *])'
         else:
             open_end_query = '((*:* NOT extras_temporal_end:[* TO *]) AND extras_temporal_start:[* TO {end_date}])'.format(
-                    end_date=end_date)
+                end_date=end_date)
 
         fq = u'{fq} ({start_query} OR {end_query} {enclosing_query} OR {open_end_query})'.format(
             fq=fq, start_query=start_query, end_query=end_query, enclosing_query=enclosing_query, open_end_query=open_end_query)
-        
 
         search_params['fq'] = fq
 
         return search_params
 
-    scores = [ ['0OL'], ['0OL','1RE'], ['0OL','1RE','2OF'], ['0OL','1RE','2OF','3URI'], ['0OL','1RE','2OF','3URI','4LD']]
+    scores = [['0OL'], ['0OL', '1RE'], ['0OL', '1RE', '2OF'], [
+        '0OL', '1RE', '2OF', '3URI'], ['0OL', '1RE', '2OF', '3URI', '4LD']]
+
     def map_qa_score(self, dict_pkg):
         if 'validated_data_dict' in dict_pkg and 'openness_score' in dict_pkg['validated_data_dict']:
-                d = json.loads(dict_pkg['validated_data_dict'])
-                score = -1
-                for r in d['resources']:
-                    if 'qa' in r:
-                        i = r['qa'].find('openness_score')
-                        s = int(r['qa'][i+17])
-                        if s > score:
-                                score=s
-                if score > 0:
-                    dict_pkg['openness']=OdshPlugin.scores[score-1]
+            d = json.loads(dict_pkg['validated_data_dict'])
+            score = -1
+            for r in d['resources']:
+                if 'qa' in r:
+                    i = r['qa'].find('openness_score')
+                    s = int(r['qa'][i+17])
+                    if s > score:
+                        score = s
+            if score > 0:
+                dict_pkg['openness'] = OdshPlugin.scores[score-1]
 
+    # @precondition.not_on_slave
 
-    #@precondition.not_on_slave
     def before_index(self, dict_pkg):
         # make special date fields solr conform
         fields = ["issued", "temporal_start", "temporal_end"]
@@ -518,11 +538,11 @@ class OdshPlugin(plugins.SingletonPlugin, DefaultTranslation, DefaultDatasetForm
             field = 'extras_' + field
             if field in dict_pkg and dict_pkg[field]:
                 d = parse(dict_pkg[field])
-                dict_pkg[field] = '{0.year:04d}-{0.month:02d}-{0.day:02d}T00:00:00Z'.format(d)
+                dict_pkg[field] = '{0.year:04d}-{0.month:02d}-{0.day:02d}T00:00:00Z'.format(
+                    d)
         # if 'res_format' in dict_pkg:
         #     dict_pkg['res_format']=[e.lower() for e in dict_pkg['res_format']]
 
         self.map_qa_score(dict_pkg)
 
         return dict_pkg
-
---
+++ ckanext/odsh/tests/test_datarequest.py
@@ -0,0 +1,71 @@
+
+from ckanext.odsh.tests.test_helpers import AppProxy
+import ckanext.odsh.tests.test_helpers as testhelpers
+import ckan.tests.factories as factories
+import uuid
+import pdb
+from ckanext.odsh.tests.harvest_sever_mock import HarvestServerMock
+import ckanext.odsh.tests.harvest_sever_mock as harvest_sever_mock
+import subprocess
+import re
+
+markdown = \
+    """
+Lorem markdownum supplex iniquis, nec nostram nam conde tympana, deae Mutinae
+regna sepulcro; morae arces quae, pia? Rates Circe, quandoquidem Ausoniae, me,
+cacumine apta, saevus abductas navigiis. Sacri ostendit *ad anas* amores nostras
+[currebam celeres], milia gaudet eripitur superest circumque auras, [nec]. Si
+haurit geminis agendum profana lacertis infamis?
+
+> Dedit potuit perenni nesciet flumine. Et sui **ibis** mihi supponat, flamina
+> mihi rogos, deus manum ora tenebras. Acta nec dominus aenum, haud de ripa
+> instabilemque amnis erat nam Patraeque parabat quod membra quamquam.
+"""
+
+
+class TestDatarequest:
+
+    def test_nologin_cannot_create_request(self):
+        pass
+
+    def _create_request(self):
+        guid = str(uuid.uuid4())
+        self._get_app().login()
+        response = self.app.get('/datarequest/new')
+        form = response.forms[0]
+        title = 'datarequest_' + guid
+        form['title'] = title
+        form['description'] = markdown
+        final_response = self.app.submit_form(form)
+
+        id = re.search(
+            '/datarequest/comment/([a-zA-Z0-9\-]*)">', final_response.body).group(1)
+        return id
+
+    def test_create_datarequest(self):
+        # Act
+        id = self._create_request()
+
+        # Assert
+        response = self.app.get('/datarequest')
+        assert id in response
+
+    def test_comment_datarequest(self):
+        # Arrange
+        id = self._create_request()
+        guid = str(uuid.uuid4())
+
+        # Act
+        response = self.app.get('/datarequest/comment/'+id)
+        form = response.forms[0]
+        form['comment'] = markdown + guid
+        final_response = self.app.submit_form(form)
+
+        # Assert
+        assert guid in final_response
+
+    def _get_app(self):
+        if not hasattr(self, 'app'):
+            app = AppProxy()
+            self.app = app
+        return self.app
